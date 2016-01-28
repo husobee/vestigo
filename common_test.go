a1ee01d6e66a00bd09f69d603d38e5907a0208ae
Ben Huson <husobee@e-ie.io>
2016-01-28 07:34:13 -0500

    bringing test coverage back up

--- common_test.go
+++ common_test.go
@@ -45,6 +45,126 @@ func TestAddRoute(t *testing.T) {
 	}
 }
 
+func TestCorsFlightWildcardOrigin(t *testing.T) {
+	router := NewRouter()
+	router.SetGlobalCors(&CorsAccessControl{
+		AllowOrigin:      []string{"*"},
+		AllowCredentials: true,
+		ExposeHeaders:    []string{"X-Header", "X-Y-Header"},
+		MaxAge:           3600 * time.Second,
+		AllowHeaders:     []string{"X-Header", "X-Y-Header"},
+	})
+
+	path := "/test"
+	f := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte(""))
+	}
+	router.Get(path, f)
+	router.Post(path, f)
+
+	router.SetCors(path, &CorsAccessControl{
+		AllowMethods: []string{"GET"},                    // only allow cors for this resource on GET calls
+		AllowHeaders: []string{"X-Header", "X-Z-Header"}, // Allow this one header for this resource
+	})
+
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("POST", path, nil)
+	if err != nil {
+		t.Errorf("Failed to create a new request, method: %s, path: %s", "OPTIONS", path)
+	}
+
+	// add preflight headers
+	r.Header.Add("Origin", "test.bah")
+	r.Header.Add("Access-Control-Request-Headers", "X-Y-Header")
+
+	router.ServeHTTP(w, r)
+	if w.Header().Get("Access-Control-Allow-Origin") != "*" {
+		t.Errorf("Invalid POST response, method: %s, path: %s, code: %s, body: %s", "POST", path, w.Code, w.Body.String())
+	}
+
+}
+
+func TestCorsFlightBadOrigin(t *testing.T) {
+	router := NewRouter()
+	router.SetGlobalCors(&CorsAccessControl{
+		AllowOrigin:      []string{"test.com"},
+		AllowCredentials: true,
+		ExposeHeaders:    []string{"X-Header", "X-Y-Header"},
+		MaxAge:           3600 * time.Second,
+		AllowHeaders:     []string{"X-Header", "X-Y-Header"},
+	})
+
+	path := "/test"
+	f := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte(""))
+	}
+	router.Get(path, f)
+	router.Post(path, f)
+
+	router.SetCors(path, &CorsAccessControl{
+		AllowMethods: []string{"GET"},                    // only allow cors for this resource on GET calls
+		AllowHeaders: []string{"X-Header", "X-Z-Header"}, // Allow this one header for this resource
+	})
+
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("POST", path, nil)
+	if err != nil {
+		t.Errorf("Failed to create a new request, method: %s, path: %s", "OPTIONS", path)
+	}
+
+	// add preflight headers
+	r.Header.Add("Origin", "test.bah")
+	r.Header.Add("Access-Control-Request-Headers", "X-Y-Header")
+
+	router.ServeHTTP(w, r)
+	if w.Header().Get("Access-Control-Allow-Origin") != "" {
+		t.Errorf("Invalid POST response, method: %s, path: %s, code: %s, body: %s", "POST", path, w.Code, w.Body.String())
+	}
+
+}
+
+func TestCorsFlightGood(t *testing.T) {
+	router := NewRouter()
+	router.SetGlobalCors(&CorsAccessControl{
+		AllowOrigin:      []string{"*", "test.com"},
+		AllowCredentials: true,
+		ExposeHeaders:    []string{"X-Header", "X-Y-Header"},
+		MaxAge:           3600 * time.Second,
+		AllowHeaders:     []string{"X-Header", "X-Y-Header"},
+	})
+
+	path := "/test"
+	f := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte(""))
+	}
+	router.Get(path, f)
+	router.Post(path, f)
+
+	router.SetCors(path, &CorsAccessControl{
+		AllowMethods: []string{"GET"},                    // only allow cors for this resource on GET calls
+		AllowHeaders: []string{"X-Header", "X-Z-Header"}, // Allow this one header for this resource
+	})
+
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("POST", path, nil)
+	if err != nil {
+		t.Errorf("Failed to create a new request, method: %s, path: %s", "OPTIONS", path)
+	}
+
+	// add preflight headers
+	r.Header.Add("Origin", "test.com")
+	r.Header.Add("Access-Control-Request-Headers", "X-Y-Header")
+
+	router.ServeHTTP(w, r)
+	if w.Code != 200 || w.Body.String() != "" || w.Header().Get("Access-Control-Allow-Origin") != "test.com" {
+		t.Errorf("Invalid POST response, method: %s, path: %s, code: %s, body: %s", "POST", path, w.Code, w.Body.String())
+	}
+
+}
+
 func TestTrace(t *testing.T) {
 	router := NewRouter()
 	AllowTrace = true
