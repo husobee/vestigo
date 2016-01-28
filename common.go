bcde91a1fc59a022dd12d1dce56fc6bc9cab9e65
Ben Huson <husobee@e-ie.io>
2016-01-27 20:07:22 -0500

    updates to perform processing for simple cors requests, non-preflight calls

--- common.go
+++ common.go
@@ -326,6 +326,43 @@ var (
 			w.Write([]byte(""))
 		}
 	}
+	// corsFlightWrapper - Wrap the handler in cors
+	corsFlightWrapper = func(gcors *CorsAccessControl, lcors *CorsAccessControl, allowedMethods string, f func(http.ResponseWriter, *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+		return func(w http.ResponseWriter, r *http.Request) {
+
+			if origin := r.Header.Get("Origin"); origin != "" {
+				cors := gcors.Merge(lcors)
+				if cors != nil {
+					// validate origin is in list of acceptable allow-origins
+					allowedOrigin := false
+					allowedOriginExact := false
+					for _, v := range cors.GetAllowOrigin() {
+						if v == origin {
+							w.Header().Add("Access-Control-Allow-Origin", origin)
+							allowedOriginExact = true
+							allowedOrigin = true
+							break
+						}
+					}
+					if !allowedOrigin {
+						for _, v := range cors.GetAllowOrigin() {
+							if v == "*" {
+								w.Header().Add("Access-Control-Allow-Origin", v)
+								allowedOrigin = true
+								break
+							}
+						}
+					}
+
+					// if allow credentials is allowed on this resource respond with true
+					if allowCredentials := cors.GetAllowCredentials(); allowedOriginExact && allowCredentials {
+						w.Header().Add("Access-Control-Allow-Credentials", "true")
+					}
+				}
+			}
+			f(w, r)
+		}
+	}
 
 	// optionsHandler - Generic Options Handler to handle when method isn't allowed for a resource
 	optionsHandler = func(gcors *CorsAccessControl, lcors *CorsAccessControl, allowedMethods string) func(w http.ResponseWriter, r *http.Request) {
