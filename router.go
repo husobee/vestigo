727cc098b7a2425c40f4a94984ff694ef2c189e6
Johnnie Kearse III <jkearse3@gmail.com>
2016-02-12 21:08:47 -0500

    Fixed Router.Handle and Router.HandleFunc

--- router.go
+++ router.go
@@ -92,7 +92,7 @@ func (r *Router) Trace(path string, handler http.HandlerFunc) {
 // Handle - Helper method to add all HTTP Methods to router
 func (r *Router) Handle(path string, handle http.Handler) {
 	for _, v := range methods {
-		if v != "HEAD" || v != "OPTIONS" {
+		if v != "HEAD" || v != "OPTIONS" || v != "TRACE" {
 			r.Add(v, path, handle.ServeHTTP)
 		}
 	}
@@ -101,7 +101,7 @@ func (r *Router) Handle(path string, handle http.Handler) {
 // HandleFunc - Helper method to add all HTTP Methods to router
 func (r *Router) HandleFunc(path string, handle http.HandlerFunc) {
 	for _, v := range methods {
-		if v != "HEAD" || v != "OPTIONS" {
+		if v != "HEAD" || v != "OPTIONS" || v != "TRACE" {
 			r.Add(v, path, handle)
 		}
 	}
